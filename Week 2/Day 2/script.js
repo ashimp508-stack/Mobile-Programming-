e8dda8dc2ac2b5accbc4b9e088d54e9cf89d62bd
Ashim Poudel <ashimpoudel@Ashims-MacBook-Air.local>
2026-09-14 11:43:35 +0545

    initial commit w

--- Week 2/Day 2/script.js
+++ Week 2/Day 2/script.js
@@ -1,28 +1,47 @@
+javascript
 function calculate() {
 
-    let science = Number(document.getElementById("science").value);
-    let math = Number(document.getElementById("math").value);
-    let english = Number(document.getElementById("english").value);
-    let computer = Number(document.getElementById("computer").value);
-    let nepali = Number(document.getElementById("nepali").value);
-    let social = Number(document.getElementById("social").value);
-    let accounting = Number(document.getElementById("accounting").value);
-    let economics = Number(document.getElementById("economics").value);
+    // Get marks from HTML
+    let economics = Number(document.getElementById("Econmics").value);
+    let maths = Number(document.getElementById("Maths").value);
+    let science = Number(document.getElementById("Science").value);
+    let social = Number(document.getElementById("Social").value);
+    let english = Number(document.getElementById("English").value);
+    let account = Number(document.getElementById("Account").value);
+    let nepali = Number(document.getElementById("Nepali").value);
 
-    let total = science + math + english + computer +
-        nepali + social + accounting + economics;
+    // Calculate total marks
+    let total = economics + maths + science + social +
+                english + account + nepali;
 
+    // Get result paragraph
     let result = document.getElementById("result");
-    if (total >= 640) {
-        result.innerHTML = "Total Marks: " + total + "/800<br>Result: DISTINCTION";
+
+    // Check result
+    if (total >= 560) {
+
+        result.innerHTML =
+            "Total Marks: " + total + "/700<br>" +
+            "Result: DISTINCTION";
+
         result.style.color = "green";
 
-    } else if (total >= 320) {
-        result.innerHTML = "Total Marks: " + total + "/800<br>Result: PASS";
+    } 
+    else if (total >= 350) {
+
+        result.innerHTML =
+            "Total Marks: " + total + "/700<br>" +
+            "Result: PASS";
+
         result.style.color = "blue";
 
-    } else {
-        result.innerHTML = "Total Marks: " + total + "/800<br>Result: FAIL";
+    } 
+    else {
+
+        result.innerHTML =
+            "Total Marks: " + total + "/700<br>" +
+            "Result: FAIL";
+
         result.style.color = "red";
     }
 
--- Week 2/Day 3/script.js
+++ Week 2/Day 3/script.js
@@ -20,3 +20,4 @@ function calculate(operation) {
 
 
 }
+
